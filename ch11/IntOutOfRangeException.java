21f424fb7a0d50219aff6f8d019796f9fdfd0806
krjohnn <ks91ko@gmail.com>
2013-10-15 05:08:28 +0300

    base

---
+++ ch11/IntOutOfRangeException.java
@@ -0,0 +1,15 @@
+package ch11;
+
+/*
+ *  File: IntOutOfRangeException.java
+ *  Author: Java, Java, Java
+ *  Description: This Exception subclass is invoked when an
+ *   integer value exceeds its bound. 
+ */
+
+public class IntOutOfRangeException extends Exception {
+   
+    public IntOutOfRangeException(int Bound) {
+        super("The input value exceeds the bound " + Bound);
+    }
+}
